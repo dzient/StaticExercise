a3b33e1b4de6fba1c5fb25e6e7d15a23d310638f
dzient <34776686+dzient@users.noreply.github.com>
2022-07-17 10:25:34 -0400

    Add files via upload
    First commit

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+//-----------------------------------------------------------------
+// Static Exercise
+//
+// Name: David Zientara
+// Date: 7-16-2022
+// Comments: An exercise in classes (exercise 1)
+// Added methods per the instructions
+//-----------------------------------------------------------------
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double temp;
+            bool success = false;
+            // Get a temperature from stdin; keep going until we
+            // get valid input:
+            do
+            {
+
+                Console.WriteLine("Enter temperature in Fahrenheit: ");
+                string str = Console.ReadLine();
+                success = double.TryParse(str, out temp);
+            } while (!success);
+            // Convert to Celsius, then convert back to Fahrenheit
+            var celsius = TempConverter.FahrenheitToCelsius(temp); // 20
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(celsius); //68
+
+            Console.WriteLine("After conversion:");
+            Console.WriteLine($"Celsius {celsius}");
+            Console.WriteLine($"Fahrenheit {fahrenheit}");
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+//-----------------------------------------------------------------
+// Static Exercise
+//
+// Name: David Zientara
+// Date: 7-16-2022
+// Comments: An exercise in classes (exercise 1)
+// Added methods per the instructions
+//-----------------------------------------------------------------
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        // FahrenheitToCelsius
+        // Given a temperature in Fahrenheit,
+        // function returns the result in celsius
+        // PARAMS: fahrenheit (double)
+        // RETURNS temperature in celsius (double)
+        public static double FahrenheitToCelsius(double fahrenheit)
+        {
+            var result = (fahrenheit - 32.0) / 1.8;
+
+            return result;
+        }
+        // CelsiusToFahrenheit
+        // Given a temperature in Celsius,
+        // function returns the result in Fahrenheit
+        // PARAMS: celsius (double)
+        // RETURNS temperature in Fahrensheit (double)
+        public static double CelsiusToFahrenheit(double celsius)
+        {
+            return (celsius * 9.0) / 5.0 + 32.0;
+        }
+    }
+}
